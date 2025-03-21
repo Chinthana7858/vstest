f7dd55bb5cb41105c463ce82cbefb59926c37473
Chinthana Prabhashitha <prabhashitharac.20@uom.lk>
2025-03-21 22:30:25 +0530

    connection

--- src/test/java/com/innovatesolutions/virtualschool/VirtualschoolApplicationTests.java
+++ src/test/java/com/innovatesolutions/virtualschool/VirtualschoolApplicationTests.java
@@ -2,12 +2,15 @@ package com.innovatesolutions.virtualschool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.data.mongodb.uri=mongodb+srv://chinthana:chinthana99@chinthana.ydhrjtg.mongodb.net/virtualschool?retryWrites=true&w=majority&appName=Chinthana"
+})
 class VirtualschoolApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
